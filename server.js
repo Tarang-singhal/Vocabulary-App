0146a52c1f4305fb00def5584bf1d64b76ced77a
tarang <tarangsinghal2@gmail.com>
2020-09-16 14:02:38 +0530

    integrating react with express 1.0

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 var app = express();
+var path = require("path");
 
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'ejs');
-
+// app.engine('html', require('ejs').renderFile);
+// app.set('view engine', 'ejs');
+// app.use(express.static("/client/public"));
 app.get("/api/c",(req,res)=>{
     var c=[
         {id:1,name:"tarang"},
@@ -12,6 +13,13 @@ app.get("/api/c",(req,res)=>{
     ];
     res.json(c);
 })
+
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static("client/build"));
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    })
+}
 
 
 var PORT = process.env.PORT || 5000;
