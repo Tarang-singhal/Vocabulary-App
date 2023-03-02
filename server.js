156601e3b70236e687f3fa349f54f055e4dc42e9
tarang.s <tarang.s@trell.in>
2023-03-02 20:36:03 +0530

    [Added]: loader

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require("express");
 const app = express();
 const path = require("path");
@@ -36,6 +37,11 @@ if(process.env.NODE_ENV==="production"){
         res.sendFile(path.resolve(__dirname,'client','build','index.html'));
     })
 }
+
+app.use(express.static("client/build"));
+app.get("*",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+})
 
 //Listening on PORT
 var PORT = process.env.PORT || 5000;
