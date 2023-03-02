10093409fa0b0c6b5f2ba0e2dfcc66abf45ce8ce
tarang.s <tarang.s@trell.in>
2023-03-02 22:12:33 +0530

    refactor

--- server.js
+++ server.js
@@ -29,15 +29,6 @@ app.use("/graphql", graphqlHTTP({
     graphiql: true
 }));
 
-
-//production environment rules
-if(process.env.NODE_ENV==="production"){
-    app.use(express.static("client/build"));
-    app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
-    })
-}
-
 app.use(express.static("client/build"));
 app.get("*",(req,res)=>{
     res.sendFile(path.resolve(__dirname,'client','build','index.html'));
